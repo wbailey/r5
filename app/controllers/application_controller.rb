74efc1f9032b169c60fc117d2253b00515c7ffbe
Wes Bailey <wes@sproutling.com>
2017-03-08 23:16:53 -0800

    first

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+# Main Application Controller
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: 'Hello World'
+  end
+end
